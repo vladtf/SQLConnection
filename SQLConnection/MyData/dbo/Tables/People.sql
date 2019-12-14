837a16802cf1a032babf3e2f51e34d443151c5be
vladtf <vendeta0263@gmail.com>
2019-12-14 12:20:39 +0000

    Add a new database and connected it to UI
    Add new PersonModel, helper class SQLDataAcces for
    connecting to a local database.

---
+++ SQLConnection/MyData/dbo/Tables/People.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[People]
+(
+    [FirstName] NCHAR(10) NULL, 
+    [LastName] NCHAR(10) NULL
+)
---
+++ SQLConnection/SQLConnection/Helpers/SQLDataAcces.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+using SQLConnection.Models;
+
+namespace SQLConnection.Helpers
+{
+    public class SQLDataAcces
+    {
+        public static List<PersonModel> GetPeople()
+        {
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=MyData;" +
+                "Integrated Security=True;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False;" +
+                "ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            SqlConnection db = new SqlConnection(connectionString);
+
+            List<PersonModel> output =  db.Query<PersonModel>("Select * from dbo.People").ToList();
+
+            return output;
+        }
+    }
+}
